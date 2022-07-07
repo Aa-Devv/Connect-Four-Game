1a551fba574dd2ec9379fb80fe497aa90f523a0e
Aa <108888158+Aa-Devv@users.noreply.github.com>
2022-07-07 20:06:12 +0300

    Create MoveAdder.java

---
+++ All The Codes/MoveAdder.java
@@ -0,0 +1,11 @@
+package com.example.connectfour;
+
+public class MoveAdder extends Thread implements Runnable {
+    int id;
+    Move m;
+
+    @Override
+    public void run() {
+        super.run();
+    }
+}
